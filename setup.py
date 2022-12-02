0bbea6cf534fb763e2a0942dda96ea215d24810e
Luc Giffon <perso@lucgiffon.fr>
2022-12-02 15:32:29 +0100

    create setup.py file for model classes + requirements with -e .

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+   name='discorss_models',
+   version='0.1',
+   packages=['discorss_models',],
+   license='MIT',
+   long_description="Models for DiscoRSS application.",
+)
