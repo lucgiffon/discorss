0cf608bfe016e168aa5dc8cea7ac78cc95fcfdf8
Luc Giffon <perso@lucgiffon.fr>
2022-12-03 14:10:31 +0100

    manage too big integer overflow error + redirect from home

--- website/views.py
+++ website/views.py
@@ -1,21 +1,20 @@
 from flask import Blueprint, render_template, abort  # it means this file contains a bunch of routes
+from loguru import logger
 
 from discorss_models.models import DiscordServer, LinkDiscordPub, Link
+from flask import redirect
 
 views_blueprint = Blueprint('views', __name__)
 
 
 @views_blueprint.route("/")
 def home():
-    return render_template("base.html")
+    return redirect("/1041036125894082621", code=302)
 
 
 def get_guild_name_from_id(id_guild):
     result = DiscordServer.query.filter(DiscordServer.discord_id == id_guild).first()
-    if result is None:
-        abort(404)
-    else:
-        return result.name
+    return result
 
 
 def get_lst_dict_links_from_id(id_guild):
@@ -34,9 +33,16 @@ def get_lst_dict_links_from_id(id_guild):
 
 @views_blueprint.route("/<int:id_guild>", strict_slashes=False)
 def guild_page(id_guild):
-    guild_name = get_guild_name_from_id(id_guild)
+    try:
+        guild = get_guild_name_from_id(id_guild)
+        guild_name = guild.name
+    except AttributeError:
+        logger.error(f"Requested guild id {id_guild} not in database.")
+        abort(404)
+    except OverflowError as oe:
+        logger.error(oe)
+        abort(404)
 
     lst_dict_links = get_lst_dict_links_from_id(id_guild)
-
 
     return render_template("feed_view.html", guild_name=guild_name, lst_dict_links=lst_dict_links)
